c5a29078bb581c628e7cb2a07d31a86f0bcf7346
Jesse Gador <jessegador@gmail.com>
2024-11-30 03:11:40 +0800

    Test for creating a topic

---
+++ test/Loggle.Tests/KafkaWslTests.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+
+namespace Loggle.Tests;
+public class KafkaWslTests
+{
+    private const string BootstrapServer = "localhost:9092";
+
+    [Fact]
+    public async Task CreateTopic()
+    {
+        var topicName = "wsl-demo-topic";
+
+        var config = new AdminClientConfig
+        {
+            BootstrapServers = BootstrapServer
+        };
+
+        using var client = new AdminClientBuilder(config)
+            .Build();
+
+        await client.CreateTopicsAsync(new[]
+        {
+            new TopicSpecification
+            {
+                Name = topicName,
+                NumPartitions = 3,
+                ReplicationFactor = 1
+            }
+        });
+    }
+}
