53e31d5a2bad7b2784a92f327b3aaaed3d7dbb47
Jesse Gador <jessegador@gmail.com>
2024-12-08 21:33:29 +0800

    Add unit test for reading egress options

---
+++ test/Loggle.Tests/EgressOptionsTests.cs
@@ -0,0 +1,42 @@
+﻿using Loggle.Egress;
+using Loggle.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Loggle.Tests;
+
+public class EgressOptionsTests
+{
+    [Fact]
+    public void CanReadEgressOptions()
+    {
+        var dic = new Dictionary<string, string>
+        {
+            { "Logging:Loggle:Egress:Kafka:BootstrapServers", "localhost:9092" },
+            { "Logging:Loggle:Egress:Kafka:TopicName", "demo-topic"}
+        };
+
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(dic!)
+            .Build();
+
+        var services = new ServiceCollection();
+        services.AddOptions();
+        services.AddSingleton<IConfiguration>(configuration);
+
+        services.AddLogging(builder =>
+        {
+            builder.AddLoggle();
+        });
+
+        var sp = services.BuildServiceProvider();
+
+        var options = sp.GetRequiredService<IOptionsMonitor<EgressOptions>>();
+
+        Assert.NotNull(options);
+        Assert.NotNull(options.CurrentValue);
+        Assert.Equal("localhost:9092", options!.CurrentValue!.Kafka?.BootstrapServers);
+        Assert.Equal("demo-topic", options!.CurrentValue!.Kafka?.TopicName);
+    }
+}
