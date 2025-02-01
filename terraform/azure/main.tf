6ab0da6253f50d7ba20c8c77118ba7536549a89a
Jesse Gador <jessegador@gmail.com>
2025-02-02 01:09:51 +0800

    Install docker

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -110,6 +110,24 @@ resource "azurerm_virtual_machine" "vm" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
+  connection {
+    type     = "ssh"
+    user     = "loggle"
+    password = "L0gg|3K3y"
+    host     = azurerm_public_ip.public_ip.ip_address
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y ca-certificates curl",
+      "sudo install -m 0755 -d /etc/apt/keyrings",
+      "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc",
+      "sudo chmod a+r /etc/apt/keyrings/docker.asc",
+      "sudo echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$${UBUNTU_CODENAME:-$VERSION_CODENAME}\") stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt update",
+      "sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
+    ]
+  }
 }
 
 output "vm_public_ip" {
