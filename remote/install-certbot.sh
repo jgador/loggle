a45b7051e88f54e71bf864903b349d6618dc79b8
Jesse Gador <jessegador@gmail.com>
2025-02-16 16:05:42 +0800

    Use staging for certbot

--- remote/install-certbot.sh
+++ remote/install-certbot.sh
@@ -4,4 +4,4 @@ sudo python3 -m venv /opt/certbot/
 sudo /opt/certbot/bin/pip install --upgrade pip
 sudo /opt/certbot/bin/pip install certbot
 sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
-sudo certbot certonly --standalone -d kibana.loggle.co -m certbot@loggle.co --agree-tos --no-eff-email --preferred-challenges=http-01
+sudo certbot certonly --standalone -d kibana.loggle.co -m certbot@loggle.co --agree-tos --no-eff-email --preferred-challenges=http-01 --staging
