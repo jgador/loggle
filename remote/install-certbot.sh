8fb105eef7524f0d02c137062cfb2e9af0770606
Jesse Gador <jessegador@gmail.com>
2025-02-23 12:38:15 +0800

    Send logs to otel collector with api key

--- remote/install-certbot.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo DEBIAN_FRONTEND=noninteractive apt install -y python3 python3-venv libaugeas0
-sudo python3 -m venv /opt/certbot/
-sudo /opt/certbot/bin/pip install --upgrade pip
-sudo /opt/certbot/bin/pip install certbot
-sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
-sudo certbot certonly --standalone -d kibana.loggle.co -m certbot@loggle.co --agree-tos --no-eff-email --preferred-challenges=http-01 --staging
