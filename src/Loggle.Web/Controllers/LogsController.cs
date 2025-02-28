1afe264deb988ced4204e3ace9787a6033ba249f
Jesse Gador <jessegador@gmail.com>
2025-02-28 16:03:28 +0800

    Refactor controller method

--- src/Loggle.Web/Controllers/LogsController.cs
+++ src/Loggle.Web/Controllers/LogsController.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using System.IO.Pipelines;
+using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
-using Loggle.Web.Configuration;
-using Loggle.Web.Model;
+using Google.Protobuf;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using OtlpLogs = OpenTelemetry.Proto.Logs.V1;
+using OpenTelemetry.Proto.Collector.Logs.V1;
 
 namespace Loggle.Web.Controllers;
 
@@ -15,6 +15,8 @@ namespace Loggle.Web.Controllers;
 [Route("")]
 public class LogsController : ControllerBase
 {
+    public const string ProtobufContentType = "application/x-protobuf";
+
     private readonly LogIngestionService _logIngestionService;
 
     public LogsController(LogIngestionService logIngestionService)
@@ -26,52 +28,110 @@ public class LogsController : ControllerBase
 
     [HttpPost]
     [Route("v1/logs")]
-    public async Task<IResult> IngestLogsAsync(CancellationToken cancellationToken)
+    public async Task<IResult> IngestLogsAsyncv1(CancellationToken cancellationToken)
     {
-        // Receives the protobuf
         try
         {
-            using var stream = new MemoryStream();
-            await Request.Body.CopyToAsync(stream, cancellationToken).ConfigureAwait(false);
-
-            if (stream.CanSeek)
+            if (!IsSupportedMediaType(Request.ContentType))
             {
-                stream.Seek(0, SeekOrigin.Begin);
+                Response.StatusCode = StatusCodes.Status415UnsupportedMediaType;
+                return Results.Empty;
             }
 
-            var logsData = OtlpLogs.LogsData.Parser.ParseFrom(stream);
+            var request = await MapOtlpLogsAsync(Request).ConfigureAwait(false);
+            var response = await _logIngestionService.IngestAsync(request, cancellationToken).ConfigureAwait(false);
 
-            var context = new OtlpContext
-            {
-                Options = new TelemetryLimitOptions { }
-            };
-
-            foreach (var rl in logsData.ResourceLogs)
-            {
-                var application = new OtlpApplication(context, rl.Resource);
-
-                foreach (var sl in rl.ScopeLogs)
-                {
-                    var logs = new List<OtlpLogEntry>(sl.LogRecords.Count);
-                    foreach (var record in sl.LogRecords)
-                    {
-                        var logEntry = new OtlpLogEntry(context, application, record);
-                        logs.Add(logEntry);
-                    }
-
-                    var response = await _logIngestionService
-                        .IngestAsync(
-                            logs,
-                            cancellationToken
-                        ).ConfigureAwait(false);
-                }
-            }
-
-            return Results.Ok();
+            return Result.Response(response);
         }
         catch (Exception ex)
         {
             return Results.InternalServerError(ex);
+        }
+    }
+
+    // TODO: Move this if we will be supporting traces and metrics
+    private static async Task<ExportLogsServiceRequest> MapOtlpLogsAsync(HttpRequest httpRequest)
+    {
+        const int MaxRequestSize = 1024 * 1024 * 4; // 4 MB
+
+        ReadResult result = default;
+        var message = new ExportLogsServiceRequest();
+
+        try
+        {
+            do
+            {
+                result = await httpRequest.BodyReader.ReadAsync().ConfigureAwait(false);
+
+                if (result.IsCanceled)
+                {
+                    throw new OperationCanceledException("Read call was canceled.");
+                }
+
+                if (result.Buffer.Length > MaxRequestSize)
+                {
+                    throw new BadHttpRequestException(
+                        $"The request body was larger than the max allowed of {MaxRequestSize} bytes.",
+                        StatusCodes.Status400BadRequest);
+                }
+
+                if (result.IsCompleted)
+                {
+                    break;
+                }
+                else
+                {
+                    httpRequest.BodyReader.AdvanceTo(result.Buffer.Start, result.Buffer.End);
+                }
+
+            } while (true);
+
+            message.MergeFrom(result.Buffer);
+            return message;
+        }
+        finally
+        {
+            if (!result.Equals(default(ReadResult)))
+            {
+                httpRequest.BodyReader.AdvanceTo(result.Buffer.End);
+            }
+        }
+    }
+
+    private static bool IsSupportedMediaType(string? contentType)
+    {
+        if (contentType != null && MediaTypeHeaderValue.TryParse(contentType, out var mt))
+        {
+            return string.Equals(mt.MediaType, ProtobufContentType, StringComparison.OrdinalIgnoreCase);
+        }
+
+        return false;
+    }
+
+    private sealed class Result
+    {
+        public static Result<T> Response<T>(T response) where T : IMessage => new Result<T>(response);
+    }
+
+    private sealed class Result<T> : IResult where T : IMessage
+    {
+        private readonly T _message;
+
+        public Result(T message) => _message = message;
+
+        public async Task ExecuteAsync(HttpContext httpContext)
+        {
+            if (!IsSupportedMediaType(httpContext.Request.ContentType))
+            {
+                httpContext.Response.StatusCode = StatusCodes.Status415UnsupportedMediaType;
+            }
+
+            using var ms = new MemoryStream();
+            _message.WriteTo(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            httpContext.Response.ContentType = ProtobufContentType;
+            await ms.CopyToAsync(httpContext.Response.Body).ConfigureAwait(false);
         }
     }
 }
